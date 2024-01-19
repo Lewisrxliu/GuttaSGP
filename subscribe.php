d0c9e3e3253c53241f9f110e525a88cd58cebcb7
lewis <128250203+Lewisrxliu@users.noreply.github.com>
2024-01-19 12:14:57 +0800

    updated

--- subscribe.php
+++ subscribe.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 include 'main.php';
 // Ensure post variable exists
@@ -22,29 +21,4 @@ if (isset($_POST['email'])) {
     // No post data specified
     exit('Please provide a valid email address!');
 }
-=======
-<?php
-include 'main.php';
-// Ensure post variable exists
-if (isset($_POST['email'])) {
-    // Validate email address
-    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-        exit('Please provide a valid email address!');
-    }
-    // Check if email exists in the database
-    $stmt = $pdo->prepare('SELECT * FROM subscribers WHERE email = ?');
-    $stmt->execute([ $_POST['email'] ]);
-    if ($stmt->fetch(PDO::FETCH_ASSOC)) {
-        exit('You\'re already a subscriber!');
-    }
-    // Insert email address into the database
-    $stmt = $pdo->prepare('INSERT INTO subscribers (email,date_subbed) VALUES (?,?)');
-    $stmt->execute([ $_POST['email'], date('Y-m-d\TH:i:s') ]);
-    // Output success response
-    exit('Thank you for subscribing!');
-} else {
-    // No post data specified
-    exit('Please provide a valid email address!');
-}
->>>>>>> 4977e8ae2051f1d1a8f320a4b2e9bd76308d3f91
 ?>
